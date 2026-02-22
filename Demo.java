7a2186c83ad86c9707a30a52797cbbef416e05b2
deepakyaao <deepakalok1994@gmail.com>
2026-02-22 21:12:50 +0530

    master 2 commit

--- Demo.java
+++ Demo.java
@@ -3,10 +3,11 @@ class Demo{
   public static void  main(String args[]){
 
     System.out.println("Arjun Krishna Contribution");
-<<<<<<< HEAD
+
 	System.out.println("deepakyaao Contribution");
-=======
+
 	 System.out.println("Arjun Krishna Contribution 2");
->>>>>>> 2bd175bfdbc61f1e47f1c87326936a1638daae1e
+	 
+     int b=220;
   }
 }
