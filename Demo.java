cd526126e4a2190972fdd891d8e8ec34b9d53a43
deepakyaao <deepakalok1994@gmail.com>
2026-02-23 16:49:31 +0530

    123 changes

--- Demo.java
+++ Demo.java
@@ -23,14 +23,26 @@ class Demo{
   void m1(){
 	  System.out.println("m1() method");
   }
+<<<<<<< HEAD
 
 
 
+=======
+<<<<<<< Updated upstream
+>>>>>>> parent of c2139ed (Revert "both changes")
   //task 325
 	
   void m3(){
 	  System.out.println("m3() done");
   }
+=======
+  
+  void m4(){
+	 System.out.println("m4() method");
+  }
+  
+  
+>>>>>>> Stashed changes
 
 
   
